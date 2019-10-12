fdfbcaaea6d6af3dcaa5e0741236a7860c5bb4c5
ZILosoft <zilgalvis@mail.ru>
2019-10-13 02:53:31 +0500

    add scripts

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt install -y git
+git clone https://github.com/ZILosoft/Deploy-ruby-on-Gcloud.git
+cd Deploy-ruby-on-Gcloud
+chmod +x *.sh
+source ./install_mongodb.sh
+source ./install_ruby.sh
+source ./deploy_programm.#!/bin/sh
+echo "FINISH"
